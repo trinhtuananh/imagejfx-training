fa046fc420710f47a32e0d514b6aac73df434298
tuananh93 <tuananh93@hotmail.fr>
2016-03-07 14:23:35 +0100

    Exo4 Part 1

---
+++ TodoMaven/src/main/java/de/knoplab/todomaven/DataTask.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.knoplab.todomaven;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+
+/**
+ *
+ * @author tuananh
+ */
+
+//I don't know which interface I should have implemented
+public class DataTask {
+private List<TodoTask> myList;
+
+private ObservableList<TodoTask> items ;
+    public DataTask (){
+        myList  = new ArrayList<>();
+        TodoTask t = new TodoTask("Exemple 1", false);
+        TodoTask tt = new TodoTask("Exemple 2", false);
+        myList.add(t);
+        myList.add(tt);
+        
+    }
+    
+    
+    public void addNewTask(String name)
+    {
+        TodoTask t = new TodoTask(name, false);
+        myList.add(t);
+        
+    }
+
+    public List<TodoTask> getMyList() {
+        return myList;
+    }
+
+    public void setMyList(List<TodoTask> myList) {
+        this.myList = myList;
+    }
+    
+    public List <String> getListofString(){
+        List <String> result = myList.stream().map(TodoTask::getName).collect(Collectors.toList());
+        return result;
+    }
+    public ObservableList<TodoTask> getObservableList()
+    {
+            items = FXCollections.observableArrayList(this.getMyList());
+            System.out.println("State of all tasks");
+            this.items.stream().forEach(e -> System.out.println(e.getName() + " "+e.getState()));
+            return items;
+    }
+    public void checkAll()
+    {
+        this.myList.stream().forEach(e -> e.setState(true));
+        System.out.println("State of all tasks");
+        this.items.stream().forEach(e -> System.out.println(e.getName() + " "+e.getState()));
+        
+    }
+    
+    public void deleteSelected()
+    {
+        this.myList = this.myList.stream().filter(e -> e.getState() == false).collect(Collectors.toList());
+        
+    }
+
+    
+
+    
+
+  
+
+}
